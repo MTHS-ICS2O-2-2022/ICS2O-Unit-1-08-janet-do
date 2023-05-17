416ad89a8404080650fcfdac6870f9f9c7b70e38
janet-do <124389477+janet-do@users.noreply.github.com>
2023-05-17 17:02:55 +0000

    sync

--- js/script.js
+++ js/script.js
@@ -3,6 +3,16 @@
 // Created by: Janet Do
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-UNIT-1-08-JANET-DO/sw.js", {
+    scope: "/ICS2O-UNIT-1-08-JANET-DO/",
+  })
+}
 function myButtonClicked() {
   alert("Hello, World!")
 }
